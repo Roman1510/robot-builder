52ee770bdf281aa69d45091ab3d741a20fefba66
Roman1510 <romanvinnick@gmail.com>
2022-08-23 12:57:43 +0200

    adding axios functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
     cart: [],
+    parts: null,
   },
   getters: {
     cartSaleItems(state) {
       return state.cart.filter((item) => item.head.onSale)
     },
   },
+  actions: {
+    getParts({ commit }) {
+      axios
+        .get('/api/parts')
+        .then((result) => commit('updateParts', result.data))
+        .catch(console.error)
+    },
+  },
   mutations: {
     addRobotToCart(state, robot) {
       state.cart.push(robot)
+    },
+    updateParts(state, parts) {
+      state.parts = parts
     },
   },
 })
